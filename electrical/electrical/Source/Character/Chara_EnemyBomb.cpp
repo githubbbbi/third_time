48a990f2a2b7fbe230d0224311374a6b162da9d0
hiromaru2 <k020g1118@g.neec.ac.jp>
2020-12-28 17:50:16 +0900

    ボム兵の動きの処理の完成

--- electrical/electrical/Source/Character/Chara_EnemyBomb.cpp
+++ electrical/electrical/Source/Character/Chara_EnemyBomb.cpp
@@ -1,6 +1,7 @@
 #include "DxLib.h"
 #include "Chara_EnemyBomb.h"
 #include "../Define/Define.h"
+#include "../Stage/Stage.h"
 
 Chara_EnemyBomb::Chara_EnemyBomb(float x, float y, int radius,
 								 float speed, int hp, int attackPower, int graphHandle):
@@ -29,15 +30,49 @@ void Chara_EnemyBomb::Move(float playerX, float playerY)
 
 	// ここから
 	{
-		if ( x - radius < 0 || x + radius > WIN_WIDTH )
+		// 移動処理
+		// 敵とプレイヤーのX座標が等しい時、スピードをダッシュに合わせる
+		if ( y == playerY)
 		{
-			speed *= -1;
+			if (speed > 0)
+			{
+				speed = DASH_SPEED;
+			}
+			else
+			{
+				speed = -DASH_SPEED;
+			}
+		}
+		// 違う時、スピードをノーマルに戻す
+		else
+		{
+			if (speed > 0)
+			{
+				speed = NORMAL_SPEED;
+			}
+			else
+			{
+				speed = -NORMAL_SPEED;
+			}
+
+			// X座標に変化がなくなった時にジャンプする
+			if (x == oldX)
+			{
+				CharaJump();
+			}
+
+			// 画面外に行きそうなとき、進む予定の位置に2つ並んでブロックがあった場合、方向を変える
+			if (x - radius < 0 || x + radius > WIN_WIDTH ||
+				Stage::GetMapParam(x + radius + 1, y) == e_MAP_BLOCK &&
+				Stage::GetMapParam(x + radius + 1, y - CHIP_SIZE) == e_MAP_BLOCK ||
+				Stage::GetMapParam(x - radius - 2, y) == e_MAP_BLOCK &&
+				Stage::GetMapParam(x - radius - 2, y - CHIP_SIZE) == e_MAP_BLOCK)
+			{
+				speed *= -1;
+			}
 		}
 
 		moveX += speed;
-
-		// ジャンプするとき
-		CharaJump();
 	}
 	// ここまで
 
